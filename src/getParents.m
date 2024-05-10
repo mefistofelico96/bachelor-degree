58038feef55cc5bfd3d12d2a738b9fd06c5f5026
Alessandro Ilardi Garofalo <alexilardi@gmail.com>
2024-05-10 18:12:54 +0200

    Update and rename get_parents.m to getParents.m

--- src/getParents.m
+++ src/getParents.m
@@ -1,21 +1,21 @@
-function parent_pointer = get_parents(tree, parent_pointer)
+function parent_pointer = getParents(tree, parent_pointer)
     parent_pointer(end + 1) = tree.parent;
     if (~tree.isLeaf)
         % South - West
         if (~tree.sw.isEmpty)
-            parent_pointer = get_parents(tree.sw, parent_pointer);
+            parent_pointer = getParents(tree.sw, parent_pointer);
         end
         % South - East
         if (~tree.se.isEmpty)
-            parent_pointer = get_parents(tree.se, parent_pointer);
+            parent_pointer = getParents(tree.se, parent_pointer);
         end
         % North - West
         if (~tree.nw.isEmpty)
-            parent_pointer = get_parents(tree.nw, parent_pointer);
+            parent_pointer = getParents(tree.nw, parent_pointer);
         end
         % North - East
         if (~tree.ne.isEmpty)
-            parent_pointer = get_parents(tree.ne, parent_pointer);
+            parent_pointer = getParents(tree.ne, parent_pointer);
         end
     end
 end
